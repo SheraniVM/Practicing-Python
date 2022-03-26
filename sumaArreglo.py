924612eec1638b3111fa761ec5b6f055a29a9954
Sherani Vazquez <ada_hack90@hackademy.mx>
2022-03-26 02:46:30 -0600

    Suma de arreglos OK

--- sumaArreglo.py
+++ sumaArreglo.py
@@ -1,10 +1,23 @@
-#Función que recibe de parametros x, y y devuelve la suma de x+y.
-print("Ingresa \n")
-x = int(input("x: "))
-y= int(input("y: "))
-def suma(x, y):
-    return int(x+y)
-    #defino como entero el retorno de la suma unicamente considerando 
-    #que se solicita con una función int suma(x, y), pero si se quitará el int del return seguiría funcionando
-    #porque se están pasando dos enteros
-print(suma(x,y))
+#Función que dada un arreglo de números no mayor a 1000, devuelva la suma
+#Entradas: Arreglo de números random_numbers
+#Salida: Suma de dicho arreglo suma
+
+import random
+def crearArreglo():
+    random_numbers = [random.randint(1, 1000) for x in range(1000)] 
+    '''Para modificar cuántos números queremos sumar modificar el parametro range()
+       Para modificar el rango de los numeros que apareceran en la lista modificar (1, 100).
+       Ejemplo:
+       random.randint(1, 1000) for x in range(10000)] 
+       creara una lista de 10 números enteros aleatorios entre 1 y 1000'''
+    print(random_numbers)
+    return random_numbers
+
+def sumarArreglo(lista):
+    x=0
+    suma=0
+    for x in range (len(lista)):
+        suma = lista[x]+ suma
+    return suma
+
+print(sumarArreglo(crearArreglo()))
